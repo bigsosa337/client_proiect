2cf2879a2a5229893c0c795fb1b0ca0afa51f9b1
Andrei Danila <115635392+bigsosa337@users.noreply.github.com>
2023-09-18 00:17:10 +0300

    homepage added

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
 <template>
-<div></div>
-  <div>
-      <router-view></router-view>
-  </div>
+    <div>
+        <header>
+            <h1>Image Gallery</h1>
+            <button @click="goToUpload()">Upload</button>
+        </header>
+        <div></div>
+        <div class="body">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 
-<script setup>
+<script>
+
+export default {
+    methods: {
+        goToUpload() {
+            this.$router.push('/upload');
+        }
+    }
+}
+
 
 </script>
 
-
 <style scoped>
-
+.body {
+    margin-top: 100px;
+}
 </style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,15 +1,91 @@
 <template>
-    <div>
-        <h1>Home Page</h1>
+    <div class="image-list">
+        <div class="row">
+            <div
+                v-for="(filename, index) in imageFilenames"
+                :key="index"
+                class="col-3 image-item"
+            >
+                <!-- Display the image -->
+                <img :src="getImageUrl(filename)" alt="Image" @click="goToImage(filename)" />
+                <!-- Display other image details -->
+                <div class="imageDiv">
+                    <p>{{ getImageTitle(filename) }}</p>
+                    <!-- <p>{{ getImageDescription(filename) }}</p> -->
+                    <!-- <p>Tags: {{ getImageTags(filename) }}</p> -->
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: "HomePage"
-}
+    data() {
+        return {
+            imageFilenames: [],
+        };
+    },
+    mounted() {
+        this.fetchImageFilenames();
+    },
+    methods: {
+        async fetchImageFilenames() {
+            try {
+                // Make an HTTP request to your server to fetch the list of image filenames
+                const response = await fetch('http://localhost:3000/listImages');
+                const data = await response.json();
+
+                // Update data property with the list of filenames
+                this.imageFilenames = data.images;
+            } catch (error) {
+                console.error('Error fetching image filenames:', error);
+            }
+        },
+        getImageUrl(filename) {
+            return `http://localhost:3000/getImageData/${filename}`;
+        },
+        getImageTitle(filename) {
+            return `${filename}`;
+        },
+        goToImage(filename) {
+            this.$router.push(`/details/${filename}`);
+        },
+        getImageDescription(filename) {
+            return `Description for ${filename}`;
+        },
+        goToImagePage(filename) {
+            this.$router.push(`/details/${filename}`);
+        },
+        getImageTags(filename) {
+            return 'Tag1, Tag2, Tag3'; // Example tags
+        },
+    },
+};
 </script>
 
 <style scoped>
+/* Add your component-specific styles here */
 
+.row {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between; /* Adjust as needed */
+}
+
+.col-3 {
+    width: calc(25% - 10px); /* Adjust as needed */
+    margin-bottom: 20px;
+}
+
+.image-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+.image-item img {
+    max-height: 200px;
+    aspect-ratio: 1/1;
+}
 </style>
